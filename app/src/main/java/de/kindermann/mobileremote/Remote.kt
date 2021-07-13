1a8f42f8c22d29bd821f330e70471c4cd9997550
waleed.aman <uk067188@student.uni-kassel.de>
2021-07-14 00:29:41 +0200

    added logging, removed duplicates

--- app/src/main/java/de/kindermann/mobileremote/Remote.kt
+++ app/src/main/java/de/kindermann/mobileremote/Remote.kt
@@ -1,102 +1,122 @@
 package de.kindermann.mobileremote
 
+import android.app.Instrumentation
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.google.android.material.button.MaterialButton
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.features.websocket.*
-import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.ClosedReceiveChannelException
-import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.java_websocket.client.WebSocketClient
-import org.java_websocket.handshake.ServerHandshake
-import java.net.ConnectException
-import java.net.NoRouteToHostException
-import java.net.URI
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import kotlinx.coroutines.*
+import java.net.InetSocketAddress
+import io.ktor.client.features.logging.*
+import io.ktor.http.*
+import io.ktor.http.HttpMethod.Companion.Get
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import org.json.JSONObject
+
 
 class Remote : AppCompatActivity() {
-    private lateinit var webSocketClient: WebSocketClient
+    private lateinit var client:HttpClient
     private lateinit var ip:String
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remote)
         ip= intent.getStringExtra("IP").toString()
-        initWebSocket()
+        client = HttpClient(OkHttp) {
+            install(Logging)
+        }
 
-    }
+        connect(ip,"test","/")
+        val upbtn = findViewById<MaterialButton>(R.id.btnup)
+        upbtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_DPAD_UP.toString(),"/command")
+        }
 
-    override fun onResume() {
-        super.onResume()
-        initWebSocket()
-    }
+        val leftBtn: MaterialButton = findViewById<MaterialButton>(R.id.leftBtn)
+        leftBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_DPAD_LEFT.toString(),"/command")
+        }
 
-    override fun onPause() {
-        super.onPause()
-        webSocketClient.close()
-    }
+        val rightBtn: MaterialButton = findViewById<MaterialButton>(R.id.rightBtn)
+        rightBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_DPAD_RIGHT.toString(),"/command")
+        }
 
-    private fun initWebSocket() {
-        val uri: URI = URI("ws://${ip}:4444/command")
-        Log.d("TAG", uri.toString())
-        createWebSocketClient(uri)
-    }
+        val okBtn: MaterialButton = findViewById<MaterialButton>(R.id.okBtn)
+        okBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_DPAD_CENTER.toString(),"/command")
+        }
 
-    private fun createWebSocketClient(uri: URI?) {
-        webSocketClient = object : WebSocketClient(uri) {
+        val downBtn: MaterialButton = findViewById<MaterialButton>(R.id.downBtn)
+        downBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_DPAD_DOWN.toString(),"/command")
+        }
 
-            override fun onOpen(handshakedata: ServerHandshake?) {
-                Log.d("TAG", "onOpen")
-                subscribe()
-            }
+        val menuBtn: MaterialButton = findViewById<MaterialButton>(R.id.menu)
+        menuBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_MENU.toString(),"/command")
+        }
 
-            override fun onMessage(message: String?) {
-                Log.d("TAG", "onMessage: $message")
-                //setUpBtcPriceText(message)
-            }
+        val homeBtn: MaterialButton = findViewById<MaterialButton>(R.id.home)
+        homeBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_HOME.toString(),"/command")
+        }
 
-            override fun onClose(code: Int, reason: String?, remote: Boolean) {
-                Log.d("TAG", "onClose")
-                //unsubscribe()
-            }
+        val backBtn: MaterialButton = findViewById<MaterialButton>(R.id.back)
+        backBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_BACK.toString(),"/command")
+        }
 
-            override fun onError(ex: Exception?) {
-                Log.e("createWebSocketClient", "onError: ${ex?.message}")
-            }
+        val volDown: MaterialButton = findViewById<MaterialButton>(R.id.voldown)
+        volDown.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_VOLUME_DOWN.toString(),"/command")
+        }
 
-            private fun subscribe() {
-                webSocketClient.send(
-                    "Hello"
-                )
-            }
+        val volUp: MaterialButton = findViewById<MaterialButton>(R.id.volup)
+        volUp.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_VOLUME_UP.toString(),"/command")
+        }
 
+        val muteBtn: MaterialButton = findViewById<MaterialButton>(R.id.mute)
+        muteBtn.setOnClickListener {
+            connect(ip,KeyEvent.KEYCODE_MUTE.toString(),"/command")
         }
     }
 
 
-    suspend fun sendCommand(command:String, client: HttpClient, path:String,ip:String){
-        client.ws(
-            host = ip,
-            port = 4444, path = path
-        ) { // this: DefaultClientWebSocketSession
 
-            // Send text frame.
-            //send(Frame.Text(command))
 
-            // Receive frame.
+    private suspend fun connect(ktor: HttpClient, u: Url,command:String) {
+        ktor.ws(Get, u.host, u.port, u.encodedPath) {
+            send(Frame.Text(command))
             when (val frame = incoming.receive()) {
                 is Frame.Text -> println(frame.readText())
-                is Frame.Binary -> println(frame.readBytes())
             }
         }
     }
+
+    private fun connect(ip:String,command:String,path:String) {
+        val url = Url("ws://${ip}:8080${path}")
+        val ktor = HttpClient(OkHttp) {
+            install(WebSockets)
+        }
+        CoroutineScope(Dispatchers.Default).launch{
+            connect(ktor, url,command)
+        }
+    }
+
 }
